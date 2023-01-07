e5453150c5d46a19e77d8a8839c328e2ee72dc0f
jureefitz <42006462+jureefitz@users.noreply.github.com>
2023-01-07 03:42:27 -0500

    Created main portfolio component, Added background image, Added app css border

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import Portfolio from './Portfolio';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <footer>
-      This project was coded by Juree Fitzgerald and is {" "}
-      <a href="" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
-     </footer>
+  <div className="App">
+   <div className="container">
+    <div className="appborder">
+     <Portfolio />
+     </div>
     </div>
+    <small>
+      This project was coded by Juree Fitzgerald and is {" "}
+      <a href="https://github.com/jureefitz/react-portfolio-project" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+     </small>
+  </div>
   );
 }
 
---
+++ src/Portfolio.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Portfolio.css"
+
+export default function Portfolio (){
+
+return(
+<div className="Portfolio">
+    <h1>My Portfolio</h1>
+     <ul>
+      <li>Hello!</li>
+      <li>Welcome to my projects page</li>
+      <li>My name is Juree, and I am a Front-End deveolper from United States</li>
+    </ul>
+   
+ </div>
+)
+}
