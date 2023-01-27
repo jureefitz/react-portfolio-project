7bce7b85688278a10ddea93cc58614cb73bf208a
jureefitz <42006462+jureefitz@users.noreply.github.com>
2023-01-27 18:54:45 -0500

    Added new project to Project.js component and added margin to Project.css

--- src/Projects.js
+++ src/Projects.js
@@ -26,6 +26,15 @@ export default function Projects() {
             <div class="col-sm-4">
                 <div class="card">
                 <div class="card-body">
+                    <h5 class="card-title">Vanilla Javascript</h5>
+                    <p class="card-text">Vanilla Hobby Page</p>
+                    <a href="https://www.shecodes.io/workshops/shecodes-basics-add-on-16c6bb1b-1a9f-4cee-aa6d-d1d1d96c5d76/projects/1422537" target="_blank" rel="noopener noreferrer" class="btn btn-outline-warning">View</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <div class="card-body">
                     <h5 class="card-title">React</h5>
                     <p class="card-text">React Weather App</p>
                     <a href="https://gregarious-bubblegum-7e0562.netlify.app/?" target="_blank" rel="noopener noreferrer" class="btn btn-outline-warning">View</a>
